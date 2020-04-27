94d3e7120f2eba90cf75eb58fd54a368eeddf5f1
alyarctiq <aly.khimji@artiq.ca>
2020-04-26 20:54:33 -0400

    working

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-#!/usr/bin/python3.5
-#from flask import Flask
-#from flask import render_template
-#from flask import request, redirect
-import googleapiclient.discovery
-import json
-import numpy as np
-from keras import applications
-from keras.preprocessing import image
-from PIL import Image
-import urllib.request
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -28,7 +17,6 @@ def predict_json(project, region, model, instances, version=None):
 
     if 'error' in response:
         raise RuntimeError(response['error'])
-    print response['predictions']
     return response['predictions']
 
 from io import BytesIO
@@ -36,12 +24,18 @@ import urllib
 
 def loadImage(URL):
     with urllib.request.urlopen(URL) as url:
-        img = image.load_img(BytesIO(url.read()), target_size=(299, 299))
+        img = image.load_img(BytesIO(url.read()), target_size=(224,224))
     return img
 
 def load_image(filename):
-    with open(filename) as f:
+    with open(filename,'rb') as f:
         return np.array(f.read())
+
+def decode(predictions):
+     pred_arr = np.expand_dims(np.array(predictions), axis=0)
+     decoded = decode_predictions(pred_arr, top=5)[0]
+     # convert numpy dtypes to python native types
+     return [(t[0], t[1], t[2].item()) for t in decoded]
 
 # app.route('/predict', methods=['POST'])
 def form():
@@ -49,11 +43,26 @@ def form():
     project="tf-blog"
     region="uscentral1"
     model="tfblog"
+    l0=[]
     img = loadImage("https://upload.wikimedia.org/wikipedia/commons/d/dc/Fromia_monilis_%28Seastar%29.jpg")
+    #img.save("test.jpg",optimize=True,quality=5)
     img.save("test.jpg")
     version="v3"
-    predict_json(project, region, model, [{load_image("test.jpg").tolist()}], version)
-
+    #instance=np.asarray(img)
+    #imgA = image.img_to_array(image.load_img("test.jpg",target_size=(299, 299))) / 255.
+    imgA = image.img_to_array(image.load_img("test.jpg"))
+    #payload = {
+    #        "instances": [{'input_image': imgA.tolist()}]
+    #        }
+    payload = [imgA.tolist()]
+    features=predict_json(project, region, model, payload , version)
+    #print(decode_predictions(np.array(features[0]))['prediction'][0])
+    for result in features:
+         for k, v in result.items():
+            data=np.array(v)
+    data1=data.reshape(1,1000)
+    #print(data1.shape)
+    print(decode_predictions(data1, top=5))
 
 form()
 # @app.route('/')
